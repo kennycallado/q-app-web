a949c2ceb6b1e3ab714c4eac6f631a7bc3563b35
kennycallado <kennycallado@hotmail.com>
2023-12-19 20:41:55 +0100

    TEMP Shows some information about the user in the about page

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+
+import { UserService } from '../../providers/services/user.service';
+import { ScoresService } from '../../providers/services/scores.service';
 
 @Component({
   selector: 'app-about',
@@ -6,5 +9,13 @@ import { Component } from '@angular/core';
   styleUrl: './about.component.sass'
 })
 export class AboutComponent {
+  #userSvc  = inject(UserService)
+  #scoreSvc = inject(ScoresService)
 
+  user    = this.#userSvc.user
+  scores  = this.#scoreSvc.scores
+
+  get_score(): Object[] {
+    return this.scores().map(score => score.score)
+  }
 }
