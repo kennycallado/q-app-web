db97b177d46818dd5dac85ded34c5ba7d1ec52fc
kennycallado <kennycallado@hotmail.com>
2023-12-20 08:40:08 +0100

    Remove unused import statement in media.component.ts

--- src/app/modules/resources/components/media/media.component.ts
+++ src/app/modules/resources/components/media/media.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Input, ViewChild } from '@angular/core'
+import { Component, Input, ViewChild } from '@angular/core'
 
 import { YouTubePlayer } from '@angular/youtube-player';
 
